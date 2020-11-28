a9684c23827fec47764e8f8313a48a4f62ffcd5d
Luis Perez <alfonsozere@gmail.com>
2020-11-28 02:50:18 -0430

    Temple con Servicios

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  constructor(){
-    console.log('Servicios de infoPagina listo')
+  info: any = {};
+  cargada = false;
+  constructor(private http: HttpClient){
+    //console.log('Servicios de infoPagina listo')
+
+    this.http.get('assets/data/data-page.json')
+          .subscribe( resp => {
+            this.cargada = true;
+            this.info = resp;
+            console.log(resp);
+          });
   }
 }
