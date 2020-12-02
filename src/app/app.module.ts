185f27b3b08255dd659a8cddd0572c5c7ba21b1b
Luis Perez <alfonsozere@gmail.com>
2020-12-02 02:41:33 -0430

    listo para produccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 // Rutas
-
 import { AppRoutingModule } from './app-routing.module';
 
-// Paginas
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { PortafolioComponent } from './page/portafolio/portafolio.component';
-import { AboutComponent } from './page/about/about.component';
-import { ItemComponent } from './page/item/item.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { SearchComponent } from './pages/search/search.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,8 @@ import { ItemComponent } from './page/item/item.component';
     FooterComponent,
     PortafolioComponent,
     AboutComponent,
-    ItemComponent
+    ItemComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,20 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  info: any = {};
-  cargada = false;
-  constructor(private http: HttpClient){
-    //console.log('Servicios de infoPagina listo')
 
-    this.http.get('assets/data/data-page.json')
-          .subscribe( resp => {
-            this.cargada = true;
-            this.info = resp;
-            console.log(resp);
-          });
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor( private http: HttpClient ) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
   }
+
+  private cargarInfo() {
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+
+  private cargarEquipo() {
+
+    // Leer el archivo JSON
+    this.http.get('https://angular-portafolio-a0d9d.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      // console.log(resp);
+    });
+
+
+    // this.equipo = resp
+  }
+
 }
+
+
+
